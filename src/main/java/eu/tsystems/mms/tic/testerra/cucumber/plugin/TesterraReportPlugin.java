034b13041c9f1dd9f0005eb3ece83953a74b9e2d
Sebastian Kiehne <Sebastian.Kiehne@t-systems.com>
2020-02-12 14:30:46 +0100

    removed unused EventHandler from plugin

--- src/main/java/eu/tsystems/mms/tic/testerra/cucumber/plugin/TesterraReportPlugin.java
+++ src/main/java/eu/tsystems/mms/tic/testerra/cucumber/plugin/TesterraReportPlugin.java
@@ -4,7 +4,6 @@ import cucumber.api.PickleStepTestStep;
 import cucumber.api.event.ConcurrentEventListener;
 import cucumber.api.event.EventHandler;
 import cucumber.api.event.EventPublisher;
-import cucumber.api.event.TestCaseStarted;
 import cucumber.api.event.TestStepFinished;
 import cucumber.api.event.TestStepStarted;
 import eu.tsystems.mms.tic.testframework.report.model.steps.TestStep;
@@ -13,10 +12,6 @@ import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
 import org.apache.commons.text.StringEscapeUtils;
 
 public class TesterraReportPlugin implements ConcurrentEventListener {
-
-    private EventHandler<TestCaseStarted> testCaseStartedEventHandler = event -> {
-        event.testCase.getName();
-    };
 
     private EventHandler<TestStepStarted> testStepStartedEventHandler = event -> {
         if (event.testStep instanceof PickleStepTestStep) {
