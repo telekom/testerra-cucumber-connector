3a477855b8925f93fb7b317c3f5feaefbebfd613
Sebastian Kiehne <Sebastian.Kiehne@t-systems.com>
2020-02-18 14:14:28 +0100

    removed scenario name as first Testerra TestStep

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -5,14 +5,8 @@ import eu.tsystems.mms.tic.testframework.utils.UITestUtils;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
 
 public class Hook {
-
-    @Before
-    public void init(Scenario scenario) {
-        TestStep.begin("start scenario " + scenario.getName());
-    }
 
     @AfterStep
     public void endStep(Scenario scenario) {
