da4e7ec036c3eb7c016a9f9b500672b0c18db99e
Sebastian Kiehne <Sebastian.Kiehne@t-systems.com>
2020-02-18 13:29:31 +0100

    removed automated screenshots on step start and end
    standard testerra properties should be used or users of the
    TesterraReportPlugin should implement their own Hooks for screenshots.

--- src/main/java/eu/tsystems/mms/tic/testerra/cucumber/plugin/TesterraReportPlugin.java
+++ src/main/java/eu/tsystems/mms/tic/testerra/cucumber/plugin/TesterraReportPlugin.java
@@ -4,11 +4,8 @@ import cucumber.api.PickleStepTestStep;
 import cucumber.api.event.ConcurrentEventListener;
 import cucumber.api.event.EventHandler;
 import cucumber.api.event.EventPublisher;
-import cucumber.api.event.TestStepFinished;
 import cucumber.api.event.TestStepStarted;
 import eu.tsystems.mms.tic.testframework.report.model.steps.TestStep;
-import eu.tsystems.mms.tic.testframework.utils.UITestUtils;
-import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
 import org.apache.commons.text.StringEscapeUtils;
 
 public class TesterraReportPlugin implements ConcurrentEventListener {
@@ -17,20 +14,12 @@ public class TesterraReportPlugin implements ConcurrentEventListener {
         if (event.testStep instanceof PickleStepTestStep) {
             PickleStepTestStep step = (PickleStepTestStep) event.testStep;
             TestStep.begin(StringEscapeUtils.escapeHtml4(step.getStepText()));
-            UITestUtils.takeScreenshot(WebDriverManager.getWebDriver(), true);
-        }
-    };
-
-    private EventHandler<TestStepFinished> testStepFinishedEventHandler = event -> {
-        if (event.testStep instanceof PickleStepTestStep) {
-            UITestUtils.takeScreenshot(WebDriverManager.getWebDriver(), true);
         }
     };
 
     @Override
     public void setEventPublisher(EventPublisher publisher) {
         publisher.registerHandlerFor(TestStepStarted.class, testStepStartedEventHandler);
-        publisher.registerHandlerFor(TestStepFinished.class, testStepFinishedEventHandler);
     }
 
 
