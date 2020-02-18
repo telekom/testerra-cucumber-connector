e906ab6920f0a3fca17c733e0c13b441122e1ebe
Sebastian Kiehne <Sebastian.Kiehne@t-systems.com>
2020-02-18 14:15:15 +0100

    hook for screenshot on failure checks tt.takeAutomaticScreenshot propoerty

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -1,6 +1,6 @@
 package steps;
 
-import eu.tsystems.mms.tic.testframework.report.model.steps.TestStep;
+import eu.tsystems.mms.tic.testframework.common.PropertyManager;
 import eu.tsystems.mms.tic.testframework.utils.UITestUtils;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
 import io.cucumber.core.api.Scenario;
@@ -10,7 +10,7 @@ public class Hook {
 
     @AfterStep
     public void endStep(Scenario scenario) {
-        if (scenario.isFailed()) {
+        if (scenario.isFailed() && PropertyManager.getBooleanProperty("tt.takeAutomaticScreenshot")) {
             UITestUtils.takeScreenshot(WebDriverManager.getWebDriver(), true);
         }
     }
