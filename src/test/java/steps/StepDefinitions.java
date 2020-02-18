651e88602dfc31e37eae2ec148fbe72e8dd416f2
Sebastian Kiehne <Sebastian.Kiehne@t-systems.com>
2020-02-18 14:12:53 +0100

    removed webdriver.close(), it's automatically done by Testerra

--- src/test/java/steps/StepDefinitions.java
+++ src/test/java/steps/StepDefinitions.java
@@ -2,25 +2,18 @@ package steps;
 
 import eu.tsystems.mms.tic.testframework.pageobjects.factory.PageFactory;
 import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import pages.GoogleSearchPage;
 import pages.GoogleSearchResultPage;
 
 public class StepDefinitions {
 
-    @After
-    public void closeDriver() {
-        WebDriverManager.getWebDriver().close();
-    }
-
     @When("the user searches for {string}")
     public void iSearchFor(String searchInput) {
-        GoogleSearchPage googleSearchPage = PageFactory.create(GoogleSearchPage.class, WebDriverManager.getWebDriver());
+        GoogleSearchPage googleSearchPage =
+                PageFactory.create(GoogleSearchPage.class, WebDriverManager.getWebDriver());
         googleSearchPage.searchTerm(searchInput);
     }
 
