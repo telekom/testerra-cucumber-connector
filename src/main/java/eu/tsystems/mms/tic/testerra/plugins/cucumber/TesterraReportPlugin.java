efbf928ef4fe625389d32b16ae453b3216c72d4e
mreiche <mike.reiche@t-systems.com>
2021-02-10 11:16:32 +0100

    Dont quote test steps

--- src/main/java/eu/tsystems/mms/tic/testerra/plugins/cucumber/TesterraReportPlugin.java
+++ src/main/java/eu/tsystems/mms/tic/testerra/plugins/cucumber/TesterraReportPlugin.java
@@ -7,14 +7,13 @@ import io.cucumber.plugin.event.EventHandler;
 import io.cucumber.plugin.event.EventPublisher;
 import io.cucumber.plugin.event.PickleStepTestStep;
 import io.cucumber.plugin.event.TestStepStarted;
-import org.apache.commons.text.StringEscapeUtils;
 
 public class TesterraReportPlugin implements ConcurrentEventListener {
 
     private EventHandler<TestStepStarted> testStepStartedEventHandler = event -> {
         if (event.getTestStep() instanceof PickleStepTestStep) {
             PickleStepTestStep step = (PickleStepTestStep) event.getTestStep();
-            TestStep.begin(StringEscapeUtils.escapeHtml4(step.getStep().getText()));
+            TestStep.begin(step.getStep().getText());
         }
     };
 
