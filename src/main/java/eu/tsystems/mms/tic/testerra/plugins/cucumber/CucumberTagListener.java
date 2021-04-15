793e6694e08ddf79e4d0b4f915fd38549c0c57bb
Mike Reiche <mike.reiche@t-systems.com>
2021-04-15 20:06:20 +0200

    Trace @Fails annotation recursive from stack trace

--- src/main/java/eu/tsystems/mms/tic/testerra/plugins/cucumber/CucumberTagListener.java
+++ src/main/java/eu/tsystems/mms/tic/testerra/plugins/cucumber/CucumberTagListener.java
@@ -23,10 +23,13 @@ package eu.tsystems.mms.tic.testerra.plugins.cucumber;
 
 import com.google.common.eventbus.Subscribe;
 import eu.tsystems.mms.tic.testerra.plugins.cucumber.handlers.FailsHandler;
+import eu.tsystems.mms.tic.testframework.annotations.Fails;
 import eu.tsystems.mms.tic.testframework.events.MethodEndEvent;
+import eu.tsystems.mms.tic.testframework.logging.Loggable;
 import eu.tsystems.mms.tic.testframework.report.model.context.MethodContext;
 import io.cucumber.testng.PickleWrapper;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,23 +41,60 @@ import java.util.Optional;
  * If the test was successful it Adds an info with the scenario name to the dashboard and adds a priority message to the
  * method context.
  */
-public class CucumberTagListener implements MethodEndEvent.Listener {
+public class CucumberTagListener implements MethodEndEvent.Listener, Loggable {
+
+    private final FailsHandler failsHandler = new FailsHandler();
 
     @Subscribe
     @Override
     public void onMethodEnd(MethodEndEvent event) {
         MethodContext methodContext = event.getMethodContext();
-        Optional<PickleWrapper> pickle = methodContext.getParameterValues().stream().filter(o -> o instanceof PickleWrapper).map(e -> (PickleWrapper) e).findFirst();
+        Optional<PickleWrapper> pickle = methodContext.getParameterValues().stream()
+                .filter(o -> o instanceof PickleWrapper).map(e -> (PickleWrapper) e)
+                .findFirst();
 
         pickle.ifPresent(pickleWrapper -> {
             List<String> tags = pickle.get().getPickle().getTags();
             tags.forEach(e -> handleTag(e, event));
         });
+
+        Throwable throwable = event.getTestResult().getThrowable();
+
+        if (throwable != null) {
+            findFailsAnnotationInStackTrace(throwable).ifPresent(fails -> {
+                methodContext.addAnnotation(fails);
+                failsHandler.handle(event);
+            });
+        }
+
     }
 
     private void handleTag(String tag, MethodEndEvent event) {
+        // Direct cucumber fails annotation on scenario
         if (tag.equals("@Fails")) {
-            new FailsHandler().handle(event);
+            failsHandler.handle(event);
         }
+    }
+
+    private Optional<Fails> findFailsAnnotationInStackTrace(Throwable throwable) {
+        while (throwable != null) {
+            for (StackTraceElement stackTraceElement : throwable.getStackTrace()) {
+                try {
+                    Class<?> cls = Class.forName(stackTraceElement.getClassName());
+                    for (Method declaredMethod : cls.getDeclaredMethods()) {
+                        if (declaredMethod.getName().equals(stackTraceElement.getMethodName())) {
+                            if (declaredMethod.isAnnotationPresent(Fails.class)) {
+                                return Optional.of(declaredMethod.getAnnotation(Fails.class));
+                            }
+                        }
+                    }
+                    ;
+                } catch (Exception e) {
+                    log().debug("Unable to trace " + Fails.class.getSimpleName() + " annotation");
+                }
+            }
+            throwable = throwable.getCause();
+        }
+        return Optional.empty();
     }
 }
