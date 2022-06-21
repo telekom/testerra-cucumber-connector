ec4aea066c6517e2e4624d8565f73f65f8ac5893
Martin Großmann <martin.grossmann@t-systems.com>
2022-06-21 12:53:57 +0200

    Updated Testerra Cucumber demo test class

--- src/test/java/RunTesterraCucumberTest.java
+++ src/test/java/RunTesterraCucumberTest.java
@@ -1,13 +1,31 @@
 import eu.tsystems.mms.tic.testframework.report.TesterraListener;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 
 @Listeners(TesterraListener.class)
 @CucumberOptions(
         plugin = {"eu.tsystems.mms.tic.testerra.plugins.cucumber.TesterraReportPlugin"},
         features = "src/test/resources/features/",
-        glue = {"eu.tsystems.mms.tic.testerra.test.cucumber","eu.tsystems.mms.tic.testerra.plugins.cucumber"}
+        glue = {
+                // This package contains some Testerra Cucumber additionals
+                "eu.tsystems.mms.tic.testerra.plugins.cucumber",
+                // Add your step definition classes
+                "eu.tsystems.mms.tic.testerra.test.cucumber"
+        }
 )
 public class RunTesterraCucumberTest extends AbstractTestNGCucumberTests {
+
+    /**
+     * Overwrite the default Test-NG Cucumber dataprovider if you want to run tests parallel.
+     *
+     * @return
+     */
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+
 }
